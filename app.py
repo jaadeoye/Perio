2248b7d78ada870027afe0a6170d0c0685d9da76
jaadeoye <82865605+jaadeoye@users.noreply.github.com>
2026-01-10 02:27:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ with st.form(key='periodontitis'):
     age = st.number_input('Age', key='p_age', min_value=0, max_value=120,  step=1, format="%d")
     gender = st.number_input('Gender', key='p_gender', min_value=0, max_value=5,  step=1, format="%d") 
     Q2 = st.number_input('Health of teeth and gums', key='p_Q2', min_value=0, max_value=120,  step=1, format="%d")   
-    Q4 = st.number_input('Loose teeth without injury', key='p_Q4', key='p_Q2', min_value=0, max_value=120,  step=1, format="%d")   
+    Q4 = st.number_input('Loose teeth without injury', key='p_Q4', min_value=0, max_value=120,  step=1, format="%d")   
     st.form_submit_button('Calculate risk')
 
 resulta = ((1.406 * Q2) + (1.659 * Q4) + (0.105 * age) + (0.834 * gender) - 4.431)
@@ -47,11 +47,11 @@ st.write('**:violet[Severe Periodontitis]**')
 
 with st.form(key='severeperiodontitis'):
     age = st.number_input('Age', key='s_age', min_value=0, max_value=120,  step=1, format="%d")
-    smoke = st.number_input('Smoking', key='s_smoke', key='p_Q2', min_value=0, max_value=5,  step=1, format="%d")  
-    Q1 = st.number_input('Gum disease', key='s_Q1', key='p_Q2', min_value=0, max_value=5,  step=1, format="%d")
-    Q2 = st.number_input('Health of teeth and gums', key='s_Q2', key='p_Q2', min_value=0, max_value=120,  step=1, format="%d")   
-    Q4 = st.number_input('Loose teeth without injury', key='s_Q4', key='p_Q2', min_value=0, max_value=120,  step=1, format="%d")
-    Q6 = st.number_input('Teeth do not look right', key='s_Q6', key='p_Q2', min_value=0, max_value=120,  step=1, format="%d")     
+    smoke = st.number_input('Smoking', key='s_smoke', min_value=0, max_value=5,  step=1, format="%d")  
+    Q1 = st.number_input('Gum disease', key='s_Q1', min_value=0, max_value=5,  step=1, format="%d")
+    Q2 = st.number_input('Health of teeth and gums', key='s_Q2', min_value=0, max_value=120,  step=1, format="%d")   
+    Q4 = st.number_input('Loose teeth without injury', key='s_Q4', min_value=0, max_value=120,  step=1, format="%d")
+    Q6 = st.number_input('Teeth do not look right', key='s_Q6', min_value=0, max_value=120,  step=1, format="%d")     
     st.form_submit_button('Calculate risk')
 
 resulta = ((1.055 * Q1) + (1.778 * Q2) + (1.142 * Q4) + (1.380 * Q6) + (0.130 * age) + (2.110 * smoke) - 9.785)
