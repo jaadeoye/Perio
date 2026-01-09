c8cc9394ef9332255308fd9dc0b346f418c506ac
jaadeoye <82865605+jaadeoye@users.noreply.github.com>
2026-01-10 00:36:47 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+with st.form(key='form1'):
+    a = st.number_input('a',value=1.)
+    b = st.number_input('b',value=2.)    
+    st.form_submit_button('press to calculate')
+
+result = a + b**2
+if result < 0 :
+    st.error('negative result')
+else:
+    st.write(f'result is {result}')
