59983dadd12cb0c75563ee42b12f68eb10094275
jaadeoye <82865605+jaadeoye@users.noreply.github.com>
2026-01-10 01:10:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,13 @@ st.markdown('####')
 st.write('**:violet[Periodontitis]**')
 
 with st.form(key='periodontitis'):
-         Gum disease = st.number_input('Q1',value=0.)
-    Health of teeth and gums = st.number_input('Q2',value=0.)  
+    Age = st.number_input('age',value=0.)
+    Smoking = st.number_input('smoke',value=0.)
+    Gender = st.number_input('gender',value=0.)   
     Gum disease = st.number_input('Q1',value=0.)
-    Health of teeth and gums = st.number_input('Q2',value=0.)    
+    Health of teeth and gums = st.number_input('Q2',value=0.)   
+    Loose teeth without injury = st.number_input('Q4',value=0.)
+    Teeth do not look right = st.number_input('Q6',value=0.)     
     st.form_submit_button('press to calculate')
 
 result = a + b**2
